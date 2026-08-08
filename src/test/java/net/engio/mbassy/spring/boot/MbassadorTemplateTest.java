1e2fead804a0715fae11545c242e394cea61b588
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:14 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/net/engio/mbassy/spring/boot/MbassadorTemplateTest.java
+++ src/test/java/net/engio/mbassy/spring/boot/MbassadorTemplateTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link MbassadorTemplate }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("MbassadorTemplate Tests")
--- src/test/java/net/engio/mbassy/spring/boot/hooks/MBassadorShutdownHookTest.java
+++ src/test/java/net/engio/mbassy/spring/boot/hooks/MBassadorShutdownHookTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link MBassadorShutdownHook }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("MBassadorShutdownHook Tests")
